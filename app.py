70cc1efe9e0b42b8f6e88c1a0670ce08339a3d62
irfan rasheed <irfanrasheed273@gmail.com>
2023-09-08 18:11:36 +0530

    All required endpoints

--- app.py
+++ app.py
@@ -1,14 +1,106 @@
 from config import db
-from flask import Flask
+from flask import Flask,jsonify,request
+import uuid
 
 users = db['users']
 app = Flask(__name__)
 
 @app.route('/users', methods=['GET'])
-def get_users():
-    # Get all users details from the database
-    users_list = list(users.find({}, {'_id': 0}))
-    return users_list
+def get_all_users():
+    try:
+        # Retrieve all users from the 'users' collection
+        user_list = list(users.find())
+        
+        # Return the list of users as JSON
+        return jsonify({"users": user_list})
+    except Exception as e:
+        # Handle any errors that may occur (e.g., database connection issues)
+        return jsonify({"message": str(e)}), 500
+
+@app.route('/users/<user_id>', methods=['GET'])
+def get_user(user_id):
+    try:
+        # Get user data by id
+        user = users.find_one({"_id": user_id})
+        
+        if user:
+            # If the user is found, return the user data as JSON
+            return jsonify({"user": user})
+        else:
+            # If the user is not found, return a 404 error
+            return jsonify({"message": "User not found"}), 404
+    except Exception as e:
+        # Return error
+        return jsonify({"message": str(e)}), 500
+
+# Validate user data
+def validate_user_data(user_data):
+    required_fields = ["name", "email", "password"]
+    
+    # Check if all required fields are present in the data
+    for field in required_fields:
+        if field not in user_data:
+            return False, f"Missing field: {field}"
+    
+    return True, None
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    try:
+        # Get user data from the request JSON
+        user_data = request.get_json()
+
+        # Validate the user data
+        is_valid, validation_message = validate_user_data(user_data)
+
+        if not is_valid:
+            return jsonify({"message": f"Invalid user data. {validation_message}"}), 400
+
+        # Generate a unique UUID for the user
+        user_id = str(uuid.uuid4())
+
+        # Add the user ID to the user data
+        user_data["_id"] = user_id
+
+        # Insert the new user into the 'users' collection
+        result = users.insert_one(user_data)
+
+        # Return the ID of the newly created user
+        return jsonify({"message": "User created", "user_id": user_id}), 201
+    except Exception as e:
+        # Handle any errors that may occur (e.g., database connection issues)
+        return jsonify({"message": str(e)}), 500
+
+@app.route('/users/<user_id>', methods=['PUT'])
+def update_user(user_id):
+    try:
+        # Get updated user data from the request JSON
+        updated_data = request.get_json()
+        
+        # Update the user with the specified ID with the new data
+        result = users.update_one({"_id": user_id}, {"$set": updated_data})
+        
+        if result.modified_count == 1:
+            return jsonify({"message": "User updated"})
+        else:
+            return jsonify({"message": "User not found"}), 404
+    except Exception as e:
+        # Handle any errors that may occur (e.g., database connection issues)
+        return jsonify({"message": str(e)}), 500
+
+@app.route('/users/<user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    try:
+        # Delete the user with the specified ID
+        result = users.delete_one({"_id": user_id})
+        
+        if result.deleted_count == 1:
+            return jsonify({"message": "User deleted"})
+        else:
+            return jsonify({"message": "User not found"}), 404
+    except Exception as e:
+        # Handle any errors that may occur (e.g., database connection issues)
+        return jsonify({"message": str(e)}), 500
 
 if __name__=='__main__':
     app.run(debug=True)
